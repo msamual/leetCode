e4f4dad314ea5ed3ef91f614e9331025c58df587
Сергей Уряшев <suryashev@bk.ru>
2023-09-09 08:54:01 +0300

    firstCommit

---
+++ LongestPalindrome/LongestPalindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+	public partial class Solution
+	{
+		public string LongestPalindrome(string s)
+		{
+			int startPos = 0, len = 0;
+			for (int i = 0; i < s.Length; i++)
+			{
+				for (int j = 0; j <= i && j + i < s.Length && s[i - j] == s[i + j]; ++j)
+				{
+					if (len < j * 2 + 1)
+					{
+						startPos = i - j;
+						len = j * 2 + 1;
+					}
+				}
+				for (int j = 0; j <= i && j + i < s.Length - 1 && s[i - j] == s[i + j + 1]; ++j)
+				{
+					if (len < (j + 1) * 2)
+					{
+						startPos = i - j;
+						len = (j + 1) * 2;
+					}
+				}
+			}
+			return s.Substring(startPos, len);
+		}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace leetCode
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				string input = Console.ReadLine();
+				Console.WriteLine(new Solution().LongestPalindrome(input));
+			}
+		}
+	}
+}
