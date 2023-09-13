ee1b3aabc02afdda8a660f07e504685976084063
sergeyu <Sergey.Uryashev@esterdev.com>
2023-09-13 10:34:40 +0300

    ZigZagConversion Better Solve

--- ZigZagConversion/Solution.cs
+++ ZigZagConversion/Solution.cs
@@ -12,37 +12,20 @@ namespace leetCode.ZigZagConversion
 		{
 			if (numRows == 1 || s.Length <= numRows)
 				return s;
-			char[] res = new char[s.Length];
-			int[] rowIndexes = new int[numRows];
+			StringBuilder res = new StringBuilder(s.Length);
 			int period = numRows * 2 - 2;
-			rowIndexes[0] = 0;
-			int waveCount = s.Length / period, tail = s.Length % period;
-			for (int i = 1; i < numRows; i++)
+			for (short row = 0; row < numRows; ++row)
 			{
-				if (i == 1)
-				{
-					rowIndexes[i] = waveCount;
-					rowIndexes[i] += tail > 0 ? 1 : 0;
-				}
-				else
-				{
-					rowIndexes[i] = waveCount * 2 + rowIndexes[i - 1];
-					if (tail > period - i + 1)
-						rowIndexes[i] += 2;
-					else if (tail >= i)
-						rowIndexes[i]++;
-				}
-			}
-			int curRow = 0, dir = -1;
-			for (int i = 0; i < s.Length; ++i)
-			{
-				if (curRow == 0 || curRow == numRows - 1)
-					dir *= -1;
-				res[rowIndexes[curRow]++] = s[i];
-				curRow += dir;
-			}
+				int inc = 2 * row;
 
-			return new string(res);
+				for (int i = row; i < s.Length; i += inc)
+				{
+					res.Append(s[i]);
+					if (inc != period)
+						inc = period - inc;
+				}
+			}
+			return res.ToString();
 		}
 	}
 }
